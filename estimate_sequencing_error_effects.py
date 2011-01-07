769f1184e24352bc104164ecdef997e032831329
Brant Faircloth <brant.faircloth@gmail.com>
2011-01-07 13:49:31 -0800

    updating sequencing error effects

--- estimate_sequencing_error_effects.py
+++ estimate_sequencing_error_effects.py
@@ -26,6 +26,10 @@ def interface():
     p.add_option('--error-rate', dest = 'error', action='store', 
 type='float', default = 0.01, help='The error rate')
 
+    p.add_option('--output', dest = 'output', action='store', 
+type='string', default = None, help='The path to the output file.', 
+metavar='FILE')
+
     p.add_option('--read-length', dest = 'read', action='store', 
 type='int', default = 250, help='The read length')
 
@@ -35,25 +39,43 @@ type='int', default = 500000, help='The read count')
     p.add_option('--barcode-length', dest = 'barcode', action='store', 
 type='int', default = 10, help='The read count')
 
+    p.add_option('--iterations', dest = 'iterations', action='store', 
+type='int', default = 100, help='The read count')
+
     p.add_option('--verbose', dest = 'verbose', action='store_true', default=False, 
 help='Print iteration')
 
     (options,arg) = p.parse_args()
+    
+    if not options.output:
+        sys.exit(2)
+    
     return options, arg 
 
-def sequence_generator(i, count, read, barcode, error_rate):
-    error_dict = defaultdict(int)
-    for i in xrange(count):
-        #pdb.set_trace()
-        read = numpy.random.binomial(1, error_rate, read)
-        errors = sum(read[10:10 + barcode])
-        error_dict[errors] += 1
-    print error_dict
+def sequence_generator():
+    result = sum(numpy.random.binomial(1, options.error, options.read)[10:10 + options.barcode])
+    return result
 
-def main():
-    options, args = interface()
-    pool = multiprocessing.Pool(7)
-    pool.map(sequence_generator, range(10), options.count, options.read, options.barcode, options.error) 
+def sequence_sum(iteration):
+    result = [str(iteration.count(x)) for x in xrange(options.barcode)]
+    return result
+    
+def runner(_):
+    run = [sequence_generator() for seq in xrange(options.count)]
+    run_summary = sequence_sum(run)
+    sys.stdout.write(".")
+    sys.stdout.flush()
+    return run_summary
 
-if __name__ == '__main__':
-    main()
+options, args = interface()
+pool = multiprocessing.Pool(7)
+outf = open(options.output, 'w')
+sys.stdout.write("Processing")
+sys.stdout.flush()
+result = pool.map(runner, range(options.iterations))
+pool.close()
+pool.join()
+for r in result:
+    outf.write("{0}\n".format(','.join(r)))
+print "\n"
+outf.close()
