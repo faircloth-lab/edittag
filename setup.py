5fbab9fe1eada2e60b4dccc8ea4cc8cefcd82254
Brant Faircloth <brant.faircloth@gmail.com>
2011-02-09 17:04:06 -0800

    update release number to rc1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 if __name__ == '__main__':
     setup(
         name='edittag',
-        version="1.0",
+        version="1.0rc1",
         description="Design and check sets of edit metric sequence tags.",
         author="Brant Faircloth",
         author_email="brant.faircloth+edittag@gmail.com ",
