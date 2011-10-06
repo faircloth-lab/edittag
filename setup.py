a7c0c4cef552da8a59a8a6d2840f20e2389d3158
Brant Faircloth <brant.faircloth@gmail.com>
2011-10-05 18:36:55 -0700

    adding link to license info and updating url

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ if __name__ == '__main__':
         description="Design and check sets of edit metric sequence tags.",
         author="Brant Faircloth",
         author_email="brant.faircloth+edittag@gmail.com ",
-        url="http://baddna.github.com/edittag/",
-        license="BSD",
+        url="http://faircloth-lab.github.com/edittag/",
+        license="http://www.opensource.org/licenses/BSD-3-Clause",
         classifiers=[
             'Development Status :: 4 - Beta',
             'Environment :: Console',
@@ -21,7 +21,7 @@ if __name__ == '__main__':
             'Programming Language :: Python',
             'Topic :: Scientific/Engineering :: Bio-Informatics',
              ],
-        requires=['NumPy (>=1.3)',],
+        requires=['numpy(>=1.3)',],
         long_description=open('README.txt').read(),
         package_data = {
                 # If any package contains *.txt or *.rst files, include them:
